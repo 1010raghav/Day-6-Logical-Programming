9ac5998f2c45bbaba0e2b6d6c9289ab43faca79f
Raghav <raghavwandre1234@gmail.com>
2021-09-24 23:10:54 +0530

    Main Program file

---
+++ Program_File.sh
@@ -0,0 +1,19 @@
+using System;
+using System.Diagnostics;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime_Factorization.Prime();
+            ReverseTheNumber.reverse();
+            Fibonacci_Series.Fibonacci();
+
+
+
+
+        }
+    }
+}
